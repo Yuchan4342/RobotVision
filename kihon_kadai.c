c7a3f1348ca27ef06be8c0afc761f0e8479c972f
岩田裕登 <jyuchan415@gmail.com>
2017-11-16 10:15:04 +0900

    changed indent

--- kihon_kadai.c
+++ kihon_kadai.c
@@ -13,19 +13,18 @@
 #define CAMERA_CENTER_H 91  //カメラサーボの垂直方向中央値（キャリブレーションに利用）
 #define CAMERA_CENTER_V 94  //カメラサーボの垂直方向中央値（キャリブレーションに利用）
 #define MOTOR_DEFAULT_L 122 //左モータのデフォルト値（キャリブレーションに利用）
-#define MOTOR_DEFAULT_R 135 //右モータのデフォルト値（キャリブレーションに利用）
+#define MOTOR_DEFAULT_R 134 //右モータのデフォルト値（キャリブレーションに利用）
 #define CAMERA_INIT_V 62    //カメラサーボの垂直方向初期値
 #define CAMERA_INIT_H 91    //カメラサーボの水平方向初期値
-#define ANGLE_UNIT 10       //カメラの角度[°]を変更する際の基準単位
+#define ANGLE_UNIT 5        //カメラの角度[°]を変更する際の基準単位
 
 #define PER_SECOND 16.04    // motor(100,100)による秒速 [cm/sec]
-#define ANG_V 55.05         // motor(100,156)のときの角速度 [°/sec]
+#define ANG_V 58            // motor(100,156)のときの角速度 [°/sec]
 
 #define CM_PER_PXS_Y 0.385              //1pxsあたりの実際の距離[cm/pxs]：y方向
 #define CM_PER_PXS_X 0.444              //1pxsあたりの実際の距離[cm/pxs]：x方向
 #define CAMERA_RANGE_LOWER_LIMIT 40.5   //カメラの認識範囲の下限（ロボットからの距離に相当）[cm]
-#define CAMERA_RANGE_UPPER_LIMIT 152.5  //カメラの認識範囲の上限（ロボットからの距離に相当）[cm]
-#define PUSH_DOWN_TARGET 15             //的を倒す時に前進する距離[cm]
+#define PUSH_DOWN_TARGET 25             //的を倒す時に前進する距離[cm]
 #define THRESHOULD_CIRCLES 10           //的が倒れたかどうかを判定する時に使う、円の個数の閾値
 
 #define SEARCHING_FOR_MARKER 0    // 探している時
@@ -44,19 +43,17 @@
 #define CASE_3 3 //長方形の状態が「左下-右上」の場合
 #define CASE_4 4 //長方形の状態が「真横」の場合
 
-////////////////////////////////////////////////////////////////////////////////
 #define RGB 3
-#define R 0 //R：赤
-#define G 1 //G：緑
-#define B 2 //B：青
+#define R 0     //R：赤
+#define G 1     //G：緑
+#define B 2     //B：青
 
 //各色HSVの最大・最小を要素とする構造体
 typedef struct colorHSV {
-	uchar minH, maxH; //色相 H の最小・最大
-	uchar minS, maxS; //鮮やかさ S の最小・最大
-	uchar minV, maxV; //明度 V の最小・最大
+	uchar minH, maxH;   //色相 H の最小・最大
+	uchar minS, maxS;   //鮮やかさ S の最小・最大
+	uchar minV, maxV;   //明度 V の最小・最大
 } colorHSV;
-////////////////////////////////////////////////////////////////////////////////
 
 double calcDistanceLA(int y);
 double calcDistanceLB(double A, double deg, int LR);
@@ -76,9 +73,9 @@ int main(int argc, char **argv)
 	CvMemStorage *storage;
 	CvSeq *circles = NULL;
 
-	IplImage* mask;      // 指定値によるmask (１チャネル)
-	IplImage* maskPT;      // 指定値による透視変換後画像のmask (１チャネル)
-	IplImage* contour;   // GetLargestContour() の結果
+	IplImage* mask;         // 指定値によるmask (１チャネル)
+	IplImage* maskPT;       // 指定値による透視変換後画像のmask (１チャネル)
+	IplImage* contour;      // GetLargestContour() の結果
 	IplImage** frames[] = {&frame, &frameHSV};
 	IplImage** framesPT[] = {&framePT, &framePTHSV};
 	contourInfo topContoursInfo[CONTOURS];
@@ -96,11 +93,10 @@ int main(int argc, char **argv)
 	camera_horizontal(CAMERA_INIT_H);   // 水平方向のカメラ角度を初期値に
 	camera_vertical(CAMERA_INIT_V);     // 垂直方向のカメラ角度を初期値に
 
-	////////////////////////////////////////////////////////////////////////////////
 	// 各色のHSV色．各自チューニングすること
 	colorHSV rgb_HSV[RGB];
 	//R
-	rgb_HSV[R].minH = 110, rgb_HSV[R].maxH = 140;
+	rgb_HSV[R].minH = 110, rgb_HSV[R].maxH = 150;
 	rgb_HSV[R].minS = 100, rgb_HSV[R].maxS = 230;
 	rgb_HSV[R].minV = 100, rgb_HSV[R].maxV = 255;
 	//G
@@ -109,10 +105,9 @@ int main(int argc, char **argv)
 	rgb_HSV[G].minV = 50, rgb_HSV[G].maxV = 255;
 	//B
 	rgb_HSV[B].minH = 0, rgb_HSV[B].maxH = 20;
-	rgb_HSV[B].minS = 70, rgb_HSV[B].maxS = 220;
+	rgb_HSV[B].minS = 70, rgb_HSV[B].maxS = 255;
 	rgb_HSV[B].minV = 50, rgb_HSV[B].maxV = 255;
 
-	////////////////////////////////////////////////////////////////////////////////
 	CvMat *map_matrix;
 	CvPoint2D32f src_pnt[4], dst_pnt[4];
 
@@ -134,6 +129,7 @@ int main(int argc, char **argv)
 		printf("not find camera\n");
 		return -1;
 	}
+    
 	// 解析速度向上のために画像サイズを下げる
 	cvSetCaptureProperty (capture, CV_CAP_PROP_FRAME_WIDTH, 320);
 	cvSetCaptureProperty (capture, CV_CAP_PROP_FRAME_HEIGHT, 240);
@@ -156,6 +152,8 @@ int main(int argc, char **argv)
 	cvSetMouseCallback("src", on_mouse, (void *)frames);
 	cvSetMouseCallback("dst", on_mouse, (void *)framesPT);
 	cvSetMouseCallback("contour", on_mouse, (void *)framesPT);
+
+	printf("準備完了\n");
 
 	while (1) {
 		// カメラからの入力画像1フレームをframeに格納
@@ -180,72 +178,72 @@ int main(int argc, char **argv)
 			          3, CV_RGB (0, 255, 0), -1, 8, 0);                         //円の描画
 			cvCircle (frame, cvPoint (cvRound (p[0]), cvRound (p[1])),
 			          cvRound (p[2]), CV_RGB (255, 0, 0), 6 - 2 * i, 8, 0);     //円の描画
-			// printf("( x , y ) : ( %d , %d )\n", cvRound (p[0]), cvRound (p[1])); //検出した円の中心座標(x,y)を表示
 		}
-		
+
 		// ウィンドウに表示
-		cvCvtColor(framePT, framePTHSV, CV_RGB2HSV); // miscountによって分ける必要はある？ない？
+		cvCvtColor(framePT, framePTHSV, CV_RGB2HSV);
 		cvShowImage("src", frame);
 		cvShowImage("dst", framePT);
 
-		////////////////////////////////////////////////////////////////////////////////
 		// 指定した色空間内の色(赤、青、緑)のみマスクする
-		if (misscount > 0) {
-			// このパターンだとmaskとcontourのサイズが異なり、エラーが起きる。
-			// GetMaskHSV(frame, mask, rgb_HSV[colors[ci]].minH, rgb_HSV[colors[ci]].maxH, rgb_HSV[colors[ci]].minS, rgb_HSV[colors[ci]].maxS, rgb_HSV[colors[ci]].minV, rgb_HSV[colors[ci]].maxV);
-			// GetLargestContour(frame, mask, contour, topContoursInfo);
-			// このパターンだとうまくいく。	
-			GetMaskHSV(framePT, maskPT, rgb_HSV[colors[ci]].minH, rgb_HSV[colors[ci]].maxH, rgb_HSV[colors[ci]].minS, rgb_HSV[colors[ci]].maxS, rgb_HSV[colors[ci]].minV, rgb_HSV[colors[ci]].maxV);
-			GetLargestContour(framePT, maskPT, contour, topContoursInfo);
-		} else {
-			GetMaskHSV(framePT, maskPT, rgb_HSV[colors[ci]].minH, rgb_HSV[colors[ci]].maxH, rgb_HSV[colors[ci]].minS, rgb_HSV[colors[ci]].maxS, rgb_HSV[colors[ci]].minV, rgb_HSV[colors[ci]].maxV);
-			GetLargestContour(framePT, maskPT, contour, topContoursInfo);
-		}
-		////////////////////////////////////////////////////////////////////////////////
+        GetMaskHSV(framePT, maskPT, rgb_HSV[colors[ci]].minH, rgb_HSV[colors[ci]].maxH, rgb_HSV[colors[ci]].minS, rgb_HSV[colors[ci]].maxS, rgb_HSV[colors[ci]].minV, rgb_HSV[colors[ci]].maxV);
+        GetLargestContour(framePT, maskPT, contour, topContoursInfo);
 
 		cvShowImage("contour", contour);
 		key = cvWaitKey(1);
-		////
+        
 		int x, y, angle, Case;
 		double theta; //FORWARD_STATE1で回転する角度
 		double LA, LB;
 		CvBox2D oblique;
 		switch (state) {
 		case SEARCHING_FOR_MARKER: // マーカーを探しているとき
-			if (1000 > topContoursInfo[0].area && topContoursInfo[0].area > 600) // マーカーが見つかった場合(長方形のサイズ850~920)
+			if (misscount >= 2 && topContoursInfo[0].area > 300) // マーカーが見つかった場合(長方形のサイズ850~920)
 			{
 				motor_stop();                       // 回転停止
 				t_start = -1;                       // 時間計測関係の変数を初期化
 				t_stop = -1;
 				state = FOUND_MARKER;
+                printf("Move into FOUND_MARKER\n");
 			}
-			else // マーカーが見つからない場合
+			if (topContoursInfo != NULL && (1000 > topContoursInfo[0].area && topContoursInfo[0].area > 600)) // マーカーが見つかった場合(長方形のサイズ850~920)
+			{
+				motor_stop();                       // 回転停止
+				t_start = -1;                       // 時間計測関係の変数を初期化
+				t_stop = -1;
+				state = FOUND_MARKER;
+                printf("Move into FOUND_MARKER\n");
+			} else // マーカーが見つからない場合
 			{
 				if (t_start == -1) time(&t_start);
-				if ((t_stop = time(NULL)) - t_start < 7)
-					motor(100, 156);    // 右(時計)周りに回転
+				if ((t_stop = time(NULL)) - t_start < 13)
+					motor(110, 146);    // 右(時計)周りに回転
 				else // 1周回ってマーカーが見つからない場合
 				{
+					motor_stop();
 					misscount++;
 					camera_vertical(CAMERA_INIT_V + ANGLE_UNIT * misscount); // マーカーが見つからなかった場合、ANGLE_UNIT * misscountずつカメラの角度をあげて再試行
 					t_start = -1;
+					usleep(500000);     //カメラの角度をあげたことをわかりやすくするため少しだけ停止させる
+					motor(100, 156);    //再度回転しながら探し始める
 				}
 			}
 			break;
 		case FOUND_MARKER: // マーカーが画面内に入ったとき, 正面(カメラの中央)に入るように向きを変える
 			oblique = topContoursInfo[0].oblique;   // 認識した物体を囲む長方形
 			x = oblique.center.x;                   // 認識した物体の画面内のx座標(0~239)
-			// y = oblique.center.y;                   // 認識した物体の画面内のy座標(0~269)
 			if (x < 110) motor(138, 118);           // 長方形が左にあるとき左に回転
 			else if (x > 130) motor(118, 138);      // 長方形が右にあるとき右に回転
 			else                                    //おおよそ正面にとらえた時
 				if (misscount > 0) {
 					camera_vertical(CAMERA_INIT_V);     // 垂直方向のカメラ角度を初期値に
-					move(CAMERA_RANGE_UPPER_LIMIT - CAMERA_RANGE_LOWER_LIMIT);
+					move(30);                           // マーカーの方向に少しだけ前進する
 					misscount = 0;                      // マーカーを見失った回数を初期化
 					state = SEARCHING_FOR_MARKER;
+                    printf("Move into SEARCHING_FOR_MARKER\n");
 				} else {
 					state = ROTATE_STATE1;
+                    printf("Move into ROTATE_STATE1\n");
 				}
 			break;
 		case ROTATE_STATE1: // 条件に応じて回転
@@ -253,11 +251,11 @@ int main(int argc, char **argv)
 			angle = oblique.angle;
 			y = oblique.center.y;                   // 認識した物体の画面内のy座標(0~269)
 			Case = judgeCase(topContoursInfo);      // マーカーの状態を判定する
-			LA = calcDistanceLA(y);                 //距離LAを計算
+			LA = calcDistanceLA(y);                 // 距離LAを計算
 			switch (Case) {
 			case CASE_1: //左上-右下
-				LB = calcDistanceLB(LA, angle, Case); //距離LBを計算
-				theta = angle;//回転する角度を計算
+				LB = calcDistanceLB(LA, angle, Case);   //距離LBを計算
+				theta = angle;                          //回転する角度を計算
 				break;
 			case CASE_2: //真正面
 				LB = 0;
@@ -269,68 +267,83 @@ int main(int argc, char **argv)
 				break;
 			case CASE_4: //真横
 				LB = LA * 1.4142;   //距離LBを計算 LAの1/sin45°=(√2)倍
-				// B = calcDistanceLB(A, angle, Case); //距離LBを計算
 				theta = 45;         //真横なのでとりあえず右に45度回転させることとする
 				break;
 			}
-			rotate(theta); //その場でthetaだけ回転
+			rotate(theta);  //その場でthetaだけ回転
 			state = FORWARD_STATE1;
+			printf("Move into FORWARD_STATE1\n");
 			break;
 		case FORWARD_STATE1: // 前進その1
-			// oblique = topContoursInfo[0].oblique; // 認識した物体を囲む長方形
-			move(LB);
+			move(LB);           //マトの真正面まで移動する
 			state = ROTATE_STATE2;
+			printf("Move into ROTATE_STATE2\n");
 			break;
-		case ROTATE_STATE2: // 回転
+		case ROTATE_STATE2:     // 回転
 			switch (Case) {
-			case CASE_1:    // 左上-右下のとき
+			case CASE_1:        // 左上-右下のとき
 				rotate(90);
 				break;
-			case CASE_3:    // 右上-左下のとき
+			case CASE_3:        // 右上-左下のとき
 				rotate(-90);
 				break;
-			case CASE_4:    // 真横のとき
+			case CASE_4:        // 真横のとき
 				rotate(-135);
 				break;
 			}
 			state = FORWARD_STATE2;
+			printf("Move into FORWARD_STATE2\n");
 			break;
 		case FORWARD_STATE2: // 前進その2
-			oblique = topContoursInfo[0].oblique;   // 認識した物体を囲む長方形
-			// x = oblique.center.x;   // 認識した物体の画面内のx座標(0~239)
-			y = oblique.center.y;                   // 認識した物体の画面内のy座標(0~269)
-			LA = calcDistanceLA(y);                 // 距離LAを計算
-			move(LA - PUSH_DOWN_TARGET);
+			if (topContoursInfo != NULL) {
+				oblique = topContoursInfo[0].oblique;   // 認識した物体を囲む長方形
+				y = oblique.center.y;                   // 認識した物体の画面内のy座標(0~269)
+				LA = calcDistanceLA(y);                 // 距離LAを計算
+			} else {
+
+			}
+			move(LA - PUSH_DOWN_TARGET);    //マトの手前まで移動する
 			state = AIMING_TARGET;
+			printf("Move into AIMING_TARGET\n");
 			break;
 		case AIMING_TARGET: // 的の狙いをしぼる
-			x = cvRound(p[0]);
-			// y = cvRound(p[1]);
-			if (x < 155) motor(138, 118);      // 的が左にあるとき左に回転
-			else if (x > 165) motor(118, 138); // 的が右にあるとき右に回転
-			else //おおよそ正面にとらえた時
-				state = FORWARD_STATE3;
+			if (circles->total >= THRESHOULD_CIRCLES) {
+				printf("円を発見！\n");
+				x = cvRound(p[0]);
+				if (x < 155) motor(138, 118);       // 的が左にあるとき左に回転
+				else if (x > 165) motor(118, 138);  // 的が右にあるとき右に回転
+                else {
+                    state = FORWARD_STATE3;         //おおよそ正面にとらえた時
+                    printf("Move into FORWARD_STATE3\n");
+                }
+			} else motor(138, 118);      // 的が見えないときは左に回転(経験則)
 			break;
 		case FORWARD_STATE3: // 前進その3
 			move(PUSH_DOWN_TARGET);
 			motor_stop();               // 停止
 			move(-PUSH_DOWN_TARGET);    //前進したぶん後退
 			state = CONFIRMATION_STATE;
+            printf("Move into CONFIRMATION_STATE\n");
 			break;
-		case CONFIRMATION_STATE: // 後退・確認
+		case CONFIRMATION_STATE: // 確認
 			if (circles->total < THRESHOULD_CIRCLES) {  // 的を倒したかの判定
 				state = END_STATE;                      //的を倒せた場合はEND_STATEへ
+                printf("Move into END_STATE\n");
+				move(-40);
 			} else {
 				state = AIMING_TARGET;                  //倒せなかった場合は再度的をしぼるところからやり直し
+                printf("Move into AIMING_TARGET\n");
 			}
 			break;
 		case END_STATE:
 			motor_stop(); // 停止
 			ci++;
-			if (ci < RGB) state = SEARCHING_FOR_MARKER;
+                if (ci < RGB) { //まだ倒していないマトが残っている場合
+                    state = SEARCHING_FOR_MARKER;   //再びマーカーを探し始めるところから
+                    printf("Move into SEARCHING_FOR_MARKER\n");
+                }
 			break;
 		}
-		////
 		if (key == 'q') break;
 	}
 
@@ -364,7 +377,7 @@ double calcDistanceLB(double LA, double deg, int Case) {
 
 //指定した距離[cm]だけロボットを動かす関数（正の値なら前進、負の値なら後退）
 void move(double distance) {
-	double move_time = distance / PER_SECOND * 1000000; //移動時間[us]の計算
+	double move_time = fabs(distance / PER_SECOND * 1000000); //移動時間[us]の計算
 	if (distance >= 0) motor(100, 100);                 //基本速度で前進
 	else motor(156, 156);                               //基本速度で後退
 	usleep(move_time);                                  //計算した移動時間分だけスリープ
